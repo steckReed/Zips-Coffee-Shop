5425454fdd6f846455c6ddbbb27e0605c7f51264
steckReed <61429429+steckReed@users.noreply.github.com>
2020-11-03 21:14:03 -0500

    huge push - preloaded images, added images + hover

--- View/Public/JS/custom-script.js
+++ View/Public/JS/custom-script.js
@@ -1,8 +1,8 @@
 // Global Variables
   var totalPrice = 0;
-  var espressoTotal = 0;
-  var coldbrewTotal = 0;
-  var latteTotal = 0;
+  var espressoPrice = 2.99;
+  var coldbrewPrice = 3.99;
+  var lattePrice = 4.99;
 // Global Variables
 
 $(document).ready(function(){
@@ -16,46 +16,12 @@ $(document).ready(function(){
     });
   // Form Submission
 
-  // Form Update
-    // Espresso Qty
-      $(document).on("change keyup blur", "#form-espresso-qty", function(){
-        var espressoQty = $('#form-espresso-qty').val();
-        var espressoPrice = 2.99;
-        espressoTotal = getCalc(espressoQty, espressoPrice);
-        getDisplay();
-      });
-    // Espresso Qty
-
-    // Coldbrew Qty
-      $(document).on("change keyup blur", "#form-coldbrew-qty", function(){
-        var coldbrewQty = $('#form-coldbrew-qty').val();
-        var coldbrewPrice = 3.99;
-        coldbrewTotal = getCalc(coldbrewQty, coldbrewPrice);
-        getDisplay();
-      });
-    // Coldbrew Qty
-
-    // Latte Qty
-      $(document).on("change keyup blur", "#form-latte-qty", function(){
-        var latteQty = $('#form-latte-qty').val();
-        var lattePrice = 4.99;
-        latteTotal = getCalc(latteQty, lattePrice);
-        getDisplay();
-      });
-    // Latte Qty
-
-    // Calc Price
-      function getCalc(qty, price){
-          calcPrice = qty * price;
-          return calcPrice;
-      }
-    // Calc Price
-
-    // Display
-      function getDisplay(){
-        totalPrice = espressoTotal + coldbrewTotal + latteTotal;
-        $('#form-total-price').val("$" + totalPrice.toFixed(2));
-      }
-    // Display
-  // Form Update
+  // On Qty change
+    // Espresso
+    $(document).on("change keyup blur", "#form-espresso-qty", function(){ getDisplayCalc(); });
+    // Coldbrew
+    $(document).on("change keyup blur", "#form-coldbrew-qty", function(){ getDisplayCalc(); });
+    // Latte
+    $(document).on("change keyup blur", "#form-latte-qty", function(){ getDisplayCalc(); });
+  // On Qty change
 });
